839415a4e9de10af2410ffedb99caabb060e1d52
Yngve <yngve@me.com>
2021-12-08 23:04:56 +0100

    Add cur,build,test,run commands to aoc

--- AoC.ps1
+++ AoC.ps1
@@ -1,7 +1,7 @@
 
 function New-Solution {
     param (
-        [Parameter(Mandatory=$true, HelpMessage="The name of the new AoC Solution, typically D04.")]
+        [Parameter(Mandatory=$true, HelpMessage="The name of the new AoC Solution, typically Day04.")]
         [string]
         $ProjectName
     )
@@ -44,16 +44,55 @@ function Publish-Lib {
     
 }
 
+function Set-Current($name) {
+    $env:AOC_CUR="$name"
+    Write-Host "Set current day to $name"
+}
+
+function Get-SolutionName($name) {
+    return $name ?? $env:AOC_CUR
+}
+
+function Run-Solution($name) {
+    $name = Get-SolutionName($name)
+
+
+    pushd $name/src
+    dotnet run
+    popd
+}
+
+function Build-Solution($name) {
+    $name = Get-SolutionName($name)
+
+    dotnet build ./$name/test/$name.Test.csproj
+}
+
+function Test-Solution($name) {
+    $name = Get-SolutionName($name)
+
+    dotnet test ./$name/test/$name.Test.csproj
+}
+
 function AoC() {
     $action = $args[0]
     switch($action) {
         "new" { New-Solution($args[1]) }
         "publish-lib" { Publish-Lib($args[1]) }
+        "cur" { Set-Current($args[1]) }
+        "build" { Build-Solution($args[1]) }
+        "run" { Run-Solution($args[1]) }
+        "test" { Test-Solution($args[1]) }
         default {
             Write-Host "Unrecognized command $($action)"
             Write-Host "Commands:"
             Write-Host "    aoc new <Name>"
             Write-Host "    aoc publish-lib <Name>"
+            Write-Host ""
+            Write-Host "    aoc cur <Name>"
+            Write-Host "    aoc build <Name>"
+            Write-Host "    aoc run <Name>"
+            Write-Host "    aoc test <Name>"
         }
     }
 }
